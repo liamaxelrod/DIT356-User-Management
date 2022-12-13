abe33eb16b904ef98d2134c324a86b77061be2f0
Wang <qianyuan@student.chalmers.se>
2022-12-13 23:37:23 +0800

    #2 add login method

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const mqtt = require('mqtt');
 require('dotenv').config();
 var mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('./models/user');
 
 // Topics
 const registerTopic = 'dentistimo/register';
+const loginTopic = 'dentistimo/login';
 
 // MQTT setup
 const port = '8883';
@@ -22,7 +24,8 @@ const client = mqtt.connect(connectUrl, {
 });
 
 // Mongo setup
-var mongoURI = process.env.MONGODB_URI;
+var mongoURI = process.env.MONGODB_URI
+    ;
 mongoose.connect(
     mongoURI,
     { useNewUrlParser: true, useUnifiedTopology: true },
@@ -42,13 +45,20 @@ client.on('connect', () => {
         console.log(`Subscribe to topic '${registerTopic}'`);
         console.log(clientId);
     });
+    client.subscribe([loginTopic], () => {
+        console.log(`Subscribe to topic '${loginTopic}'`);
+        console.log(clientId);
+    });
 });
 
 client.on('message', (topic, payload) => {
     console.log('Received Message:', topic, payload.toString());
     if (topic == registerTopic) {
         register(topic, payload);
-    } else {
+    } else if (topic == loginTopic) {
+        login(topic, payload);
+    }
+    else {
         console.log('Topic not defined in code');
     }
 });
@@ -104,4 +114,57 @@ async function register(topic, payload) {
     }
 }
 
+async function login(topic, payload) {
+    try {
+        const { email, password } = JSON.parse(payload.toString());
+
+        if (!email || !password) return client.publish(
+            'dentistimo/login-error',
+            'not all fields have been entered'
+        );
+       
+
+        const user = await User.findOne({ email });
+        console.log(user);
+
+        if (!user) return client.publish(
+            'dentistimo/login-error',
+            'User name or password error'
+        );
+       
+
+        const isMatch =  await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return client.publish(
+                'dentistimo/login-error',
+                'User name or password error'
+            );
+        }
+
+        // if (!bcrypt.compare(password, user.password)) return client.publish(
+        //     'dentistimo/login-error',
+        //     'User name or password error'
+        // );
+       
+
+        const tokens = {
+            'id': user._id,
+            'email': user.email,
+            'firstName': user.firstName,
+            'lastName': user.lastName,
+            'role': user.role
+        };
+
+        let token = jwt.sign(tokens, process.env.JWT, { 'expiresIn': 3600 });
+        console.log(token);
+
+        client.publish('dentistimo/login-success', JSON.stringify({ token: token }));
+    } catch (error) {
+        console.log('[login]', error);
+        return client.publish({
+            success: false,                
+            msg: error
+        });
+    }
+}
 console.log('running...');
